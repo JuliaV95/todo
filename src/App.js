9f1817c3bd49a1d8380dd5a2ba3604459e92d533
heyjulie95 <vanushinaulia@gmail.com>
2022-01-14 19:12:04 +0300

    first changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [taskText, setTaskText] = useState('Text')
+
+  function increment() {
+    setCount(count + 1)
+  }
+  function decrement() {
+    setCount(count - 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <h1>{taskText}</h1>
+      <input type='text' value={taskText}/>
+      <button onClick={increment}>Add</button>
+      <button onClick={decrement}>Remove</button>
     </div>
   );
 }
