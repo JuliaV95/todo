4775f8c49f6bee0a3588e5a4fa20602a9902dc84
heyjulie95 <vanushinaulia@gmail.com>
2022-02-16 23:38:52 +0300

    feat (data): fetching data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Header from "./components/header/header";
 import ToDoInput from "./components/toDoInput/toDoInput";
@@ -13,6 +13,30 @@ const ListWrapper = styled.div`
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(response => {
+      console.log(response);
+      if (response.ok) {
+        return response.json()
+      }
+      throw response;
+    })
+    .then(tasks => {
+      setTasks(tasks);
+    })
+    .catch(error => {
+      console.error("Error fetching data: ", error);
+      setError(error);
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+  }, [])
+
   const deleteItem = (id) => {
     setTasks(tasks.filter((item) => item.id !== id));
   };
